b1a98d8e255acceccf803930eadb808593f1107e
Rk-Reaz <rkreaz91@gmail.com>
2023-05-21 04:34:16 +0600

    food data change

--- index.js
+++ index.js
@@ -12,31 +12,31 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
   })
 
-
-app.get('/categories', (req, res) => {
+  app.get('/categories', (req, res) => {
     res.send(categories)
   })
 
-app.get('/food', (req, res) => {
+  app.get('/food', (req, res) => {
     res.send(food)
   })
 
-app.get('/food/:id', (req, res) => {
-    const id = parseInt(req.params.id)
-   const selectFood = food.find(n => n._id ===id);
-   res.send(selectFood)
+  app.get('/food/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const selectedFood = news.find(n => n._id === id);
+    res.send(selectedFood)
   })
-app.get('/categories/:id', (req, res) => {
-    const id = req.params.id
-   if(id === 0){
-    res.send(food)
-   }
-      else{
-        const categoryFood = food.filter(n => parseInt(n.category_id) ===id);
-        res.send(categoryFood)
-      }
+  app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    if (id === 0) {
+      res.send(food)
+    }
+    else {
+      const categoriesFood = food.filter(n => parseInt(n.category_id) === id)
+      res.send(categoriesFood)
+  
+    }
   })
-
 
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
