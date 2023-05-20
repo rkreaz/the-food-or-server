08487004c356283608d92477507f97b5b82873ac
Rk-Reaz <rkreaz91@gmail.com>
2023-05-21 01:56:11 +0600

    food data added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 
 
 const categories = require('./data/categories.json')
+const food = require('./data/food.json')
 app.use(cors())
 
 app.get('/', (req, res) => {
@@ -14,6 +15,26 @@ app.get('/', (req, res) => {
 
 app.get('/categories', (req, res) => {
     res.send(categories)
+  })
+
+app.get('/food', (req, res) => {
+    res.send(food)
+  })
+
+app.get('/food/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+   const selectFood = food.find(n => n._id ===id);
+   res.send(selectFood)
+  })
+app.get('/categories/:id', (req, res) => {
+    const id = req.params.id
+   if(id === 0){
+    res.send(food)
+   }
+      else{
+        const categoryFood = food.filter(n => parseInt(n.category_id) ===id);
+        res.send(categoryFood)
+      }
   })
 
 
